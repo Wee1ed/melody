a0cac0d9be2ce8223d0c501690414e5a06b41850
Wee1ed <89201075+Wee1ed@users.noreply.github.com>
2021-08-20 21:44:48 +0300

    Урок 4

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ $(document).ready(function () {
   var floorPath = $(".home-image path"); // каждый отдельный этаж в SVG
   var counterUp = $(".counter-up"); /*кнопка увеличения этажа */
   var counterDown = $(".counter-down"); /*кнопка уменьшение этажа */
-  
+  var modal = $(".modal");
+  var modalCloseButton = $(".modal-close-button");
+  var viewFlatsButton = $(".view-flats");
   
   // функция при наведении курсора или нажатия стрелки выбирается и подсвечивается этажи
   floorPath.on("mouseover", function () {
@@ -12,6 +14,9 @@ $(document).ready(function () {
     $(".counter").text(currentFloor);// записываем значение этажа в счетчик справа 
   });
 
+  floorPath.on("click", toggleModal);/*При клике на этаж вызвать окно*/ 
+  modalCloseButton.on("click",toggleModal);/*При клике на кнопку закрыть, закрывает окно*/ 
+  viewFlatsButton.on("click",toggleModal);
   counterUp.on("click", function () { // отслеживаем клик по кнопке вверх
   if(currentFloor <18) { //проверяем значение этажа, оно не должно быть больше 18
      currentFloor++;// прибавляем один этаж
@@ -31,4 +36,8 @@ $(document).ready(function () {
    $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
     }
   });
+  function toggleModal (){ 
+    //функция открыть-закрыть окно
+    modal.toggleClass("is-open");
+  }
 });
